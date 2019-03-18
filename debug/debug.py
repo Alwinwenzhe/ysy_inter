c8f0ae92fa74211c8b04219aedde67858d84d893
Alwinwenzhe <3065524859@qq.com>
2019-03-18 17:35:08 +0800

    调试了一生约用例accessToken偶尔报错的问题，未找到原因

--- debug/debug.py
+++ debug/debug.py
@@ -3,13 +3,8 @@
 # __Author: "Skiler Hao"
 # date: 2017/4/9 15:26
 
-
-i = 'SELECT pe.is_receive FROM property_employees pe WHERE pe.mobile=\'{0}\' AND pe.state=\'{1}\''
-l1 = ['17345888627','abcd']
-b = i.format(*l1)
-print (b)
-
-words = [1,2,3,4,5,6,7,8,9]
-runDay = '2018-05-06'
-insert_sql ='{0},insert_sql("{1}",  "{2}",  "{3}", "{4}", "{5}", "{6}", "{7}", "{8}", "{9}")'.format(runDay, *words).replace('"', "'").replace('`', '\`')
-print(insert_sql)
+data = None
+if data is None:
+	print('is')
+else:
+	print("not")
